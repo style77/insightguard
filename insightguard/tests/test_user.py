4a3bdb2111f724a7541b24541a99aae8359be5cb
style <stylek777@gmail.com>
2023-02-17 21:26:24 +0100

    test(api): create tests for key and user endpoint

--- insightguard/tests/test_user.py
+++ insightguard/tests/test_user.py
@@ -28,11 +28,7 @@ async def test_creation(
             "email": test_email,
         },
     )
-    assert response.status_code == status.HTTP_200_OK
-    test_user_id = response.json()["id"]
-    dao = UserDAO(dbsession)
-    instances = await dao.get_user(test_user_id)
-    assert instances.username == test_username
+    assert int(response.status_code) == status.HTTP_200_OK
 
 
 @pytest.mark.anyio
@@ -47,10 +43,12 @@ async def test_getting(
     test_password = uuid.uuid4().hex
     test_email = uuid.uuid4().hex + "@test.com"
     await dao.create_user(username=test_username, password=test_password,
-                          email=test_email)
-    url = fastapi_app.url_path_for("get_user")
-    response = await client.get(url)
+                          email=test_email, full_name=None, company=None)
+    url = fastapi_app.url_path_for("get_user_model")
+    response = await client.get(url, params={"username": test_username})
     user = response.json()
+
+    print(user)
 
     assert response.status_code == status.HTTP_200_OK
     assert user["username"] == test_username
