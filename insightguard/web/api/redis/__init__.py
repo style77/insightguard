0edd61f7f54545aa9e47125a0a96fa2eec6269c8
style <stylek777@gmail.com>
2023-02-19 04:20:09 +0100

    refactor(api): remove redis endpoint

--- insightguard/web/api/redis/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Redis API."""
-from insightguard.web.api.redis.views import router
-
-__all__ = ["router"]
--- insightguard/web/api/router.py
+++ insightguard/web/api/router.py
@@ -1,10 +1,9 @@
 from fastapi.routing import APIRouter
 
-from insightguard.web.api import user, monitoring, redis, key, model
+from insightguard.web.api import user, monitoring, key, model
 
 api_router = APIRouter()
 api_router.include_router(monitoring.router)
 api_router.include_router(user.router, prefix="/user", tags=["user"])
-api_router.include_router(redis.router, prefix="/redis", tags=["redis"])
 api_router.include_router(key.router, prefix="/key", tags=["key"])
 api_router.include_router(model.router, prefix="/model", tags=["model"])
